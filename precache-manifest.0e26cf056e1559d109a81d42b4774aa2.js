5822628190a00931c9acd73e9c99674753c0367e
yunglleung1 <yungl.leung1@gmail.com>
2019-07-12 14:33:12 -0400

    Updates

---
+++ precache-manifest.0e26cf056e1559d109a81d42b4774aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "a3e68af814259b9a1bb3",
+    "url": "/robofriends/static/js/main.7d6cd2c2.chunk.js"
+  },
+  {
+    "revision": "5d8d43bafb2036781ce9",
+    "url": "/robofriends/static/js/2.93ddf6f8.chunk.js"
+  },
+  {
+    "revision": "a3e68af814259b9a1bb3",
+    "url": "/robofriends/static/css/main.7abbac1c.chunk.css"
+  },
+  {
+    "revision": "02852f69bfaf3a84c7a2249f06d47b76",
+    "url": "/robofriends/index.html"
+  }
+];
